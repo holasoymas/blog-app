f0f0231368bc4225128be2411d39e49b44aefb7b
holasoymas <71905956331mahesh@gmail.com>
2025-04-11 23:29:35 +0545

    making a seperate folder for types

---
+++ types/index.ts
@@ -0,0 +1,24 @@
+export interface SignUpData {
+  name: string,
+  email: string,
+  password: string,
+}
+
+export type LoginData = Omit<SignUpData, "name">
+
+// the generic btn type 
+export interface BaseButtonProps {
+  href: string,
+  text: string,
+}
+
+// for buttons like submitting form like login, signup etc
+export interface AuthBtn { text: string }
+
+export interface Blog {
+  id: string,
+  title: string,
+  description: string,
+  image: string,
+  createdAt: Date,
+}
