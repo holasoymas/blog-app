7ef4755d8b853ea3bfb912d921b7dde1767fd335
holasoymas <71905956331mahesh@gmail.com>
2025-04-13 18:45:44 +0545

    added back btn and redirecting on 404 on blog not found

--- app/blogs/[slug]/page.tsx
+++ app/blogs/[slug]/page.tsx
@@ -1,39 +1,43 @@
 "use client";
 
-import { dummyBlogs } from "@/data/dummyBlogs";
-import { use } from "react";
-import { formatDate } from "@/app/utils/date";
 import Image from "next/image";
+import { formatDate } from "@/app/utils/date";
+import { use } from "react";
+import { BackBtn } from "@/components/Button";
+import { notFound } from "next/navigation";
+import { getItem } from "@/app/utils/storage";
+import type { Blog } from "@/types";
 
 export default function Blog({ params }: { params: Promise<{ slug: string }> }) {
+  const { slug } = use(params);
 
-  const { slug } = use(params)
+  const blogs = getItem<Blog[]>("blogs");
 
-  console.log("Parmas", params);
-  const blog = dummyBlogs.find(b => b.id === slug);
-  console.log(blog)
+  const blog = blogs?.find(b => b.id === slug);
 
-  if (!blog) return <p className="text-center mt-10 text-red-500">Blog not found</p>;
+  // if blog not found go to 404 error page 
+  if (!blog) return notFound();
 
   return (
-    <>
-      <div className="max-w-3xl mx-auto p-6 bg-white dark:bg-gray-900 text-gray-900 dark:text-white rounded-xl shadow">
+    <div className="max-w-3xl mx-auto p-6">
+
+      <BackBtn />
+
+      <div className="bg-white dark:bg-gray-900 text-gray-900 dark:text-white rounded-xl shadow-md p-6">
         <Image
           src={blog.image}
           width={1200}
           height={600}
           alt={blog.title}
           className="w-full h-64 object-cover rounded-xl"
+          priority // for preloading
         />
         <h1 className="text-3xl font-bold mt-6 mb-2">{blog.title}</h1>
         <p className="text-gray-500 dark:text-gray-400 mb-4">
           Posted on {formatDate(new Date(blog.createdAt))}
         </p>
-        <p className="text-lg leading-relaxed">
-          {blog.description}
-        </p>
+        <p className="text-lg leading-relaxed">{blog.description}</p>
       </div>
-    </>
-  )
+    </div>
+  );
 }
-
