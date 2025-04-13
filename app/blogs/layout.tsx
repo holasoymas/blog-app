b5d6dfdc5534ad65ad3ae9ceaf7b8b329a8e75d3
holasoymas <71905956331mahesh@gmail.com>
2025-04-13 18:47:27 +0545

    added common auth (isLoggedIn) on blogs base route

--- app/blogs/layout.tsx
+++ app/blogs/layout.tsx
@@ -1,9 +1,17 @@
-// app/blogs/layout.tsx
+"use client";
 import Navbar from "@/components/Navbar";
 import Footer from "@/components/Footer";
 import "../globals.css";
+import { getItem } from "../utils/storage";
+import { redirect } from "next/navigation";
 
 export default function BlogsLayout({ children }: { children: React.ReactNode }) {
+
+  const isLoggedIn = getItem<boolean>("isLoggedIn");
+  // console.log(isLoggedIn);
+  if (!isLoggedIn) redirect("/login");
+
+
   return (
     <div className="min-h-screen flex flex-col bg-white  dark:bg-black transition">
       <Navbar />
