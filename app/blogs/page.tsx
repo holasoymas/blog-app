b7a6d1d468fa3d052781f1a31cc5d033d2fec0b8
holasoymas <71905956331mahesh@gmail.com>
2025-04-13 18:48:24 +0545

    sorted function added so latest blog come first

--- app/blogs/page.tsx
+++ app/blogs/page.tsx
@@ -1,20 +1,22 @@
 "use client";
 
-import { dummyBlogs } from "@/data/dummyBlogs";
 import BlogCard from "@/components/BlogCard";
-import { LoginData } from '@/types';
 import { getItem } from '../utils/storage';
+import { Blog } from "@/types";
+import { sortBlogsByDate } from "../utils/date";
 
 const Blogs = () => {
 
-  const isLoggedIn = getItem<LoginData>("authUser");
-  console.log(isLoggedIn)
+  const blogs = getItem<Blog[]>("blogs");
+
+  const sortedBlogs = blogs ? sortBlogsByDate(blogs) : [];
+  // console.log(sortedBlogs);
 
   return (
     <main className="p-4 max-w-4xl mx-auto">
       <h1 className="text-3xl font-bold mb-6 text-gray-900 dark:text-white">Latest Blogs</h1>
       <div className="grid md:grid-cols-2 lg:grid-cols-3 gap-6">
-        {dummyBlogs.map(blog => (
+        {sortedBlogs && sortedBlogs.map(blog => (
           <BlogCard key={blog.id} blog={blog} />
         ))}
       </div>
