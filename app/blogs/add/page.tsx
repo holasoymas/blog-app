f8d491ecac213bf0c1075f151f6777af4f7b1a13
holasoymas <71905956331mahesh@gmail.com>
2025-04-14 13:19:46 +0545

    fix : adding authroute before rendering

--- app/blogs/add/page.tsx
+++ app/blogs/add/page.tsx
@@ -8,6 +8,7 @@ import { Blog } from "@/types";
 import { getItem, setItem } from "@/app/utils/storage";
 import { redirect } from "next/navigation";
 import { validateBlog } from "@/app/utils/validations";
+import AuthGuard from "@/hooks/AuthGuard";
 
 const imageOptions = [
   "/images/image1.jpg",
@@ -62,59 +63,61 @@ export default function BlogForm() {
   };
 
   return (
-    <form onSubmit={handleSubmit} className="bg-white dark:bg-gray-900 p-6  shadow-lg max-w-2xl mx-auto mt-6 mb-6">
-      <h2 className="text-2xl font-bold text-gray-900 dark:text-white mb-6">Create a New Blog</h2>
+    <AuthGuard>
+      <form onSubmit={handleSubmit} className="bg-white dark:bg-gray-900 p-6  shadow-lg max-w-2xl mx-auto mt-6 mb-6">
+        <h2 className="text-2xl font-bold text-gray-900 dark:text-white mb-6">Create a New Blog</h2>
 
-      <div>
-        <label className="block text-gray-800 dark:text-gray-200 mb-2">Title</label>
-        <input
-          type="text"
-          value={title}
-          onChange={(e) => setTitle(e.target.value)}
-          className="w-full p-3 rounded-lg border border-gray-300 dark:border-gray-700 bg-gray-50 dark:bg-gray-800 text-gray-900 dark:text-white mb-4"
-          placeholder="Enter blog title"
-          required
-        />
-        {formErrors.title && <p className="field-error">{formErrors.title}</p>}
-      </div>
+        <div>
+          <label className="block text-gray-800 dark:text-gray-200 mb-2">Title</label>
+          <input
+            type="text"
+            value={title}
+            onChange={(e) => setTitle(e.target.value)}
+            className="w-full p-3 rounded-lg border border-gray-300 dark:border-gray-700 bg-gray-50 dark:bg-gray-800 text-gray-900 dark:text-white mb-4"
+            placeholder="Enter blog title"
+            required
+          />
+          {formErrors.title && <p className="field-error">{formErrors.title}</p>}
+        </div>
 
-      <div>
-        <label className="block text-gray-800 dark:text-gray-200 mb-2">Description</label>
-        <textarea
-          value={description}
-          onChange={(e) => setDescription(e.target.value)}
-          className="w-full p-3 rounded-lg border border-gray-300 dark:border-gray-700 bg-gray-50 dark:bg-gray-800 text-gray-900 dark:text-white mb-4"
-          placeholder="Enter blog description"
-          rows={4}
-          required
-        />
-        {formErrors.description && <p className="field-error">{formErrors.description}</p>}
-      </div>
+        <div>
+          <label className="block text-gray-800 dark:text-gray-200 mb-2">Description</label>
+          <textarea
+            value={description}
+            onChange={(e) => setDescription(e.target.value)}
+            className="w-full p-3 rounded-lg border border-gray-300 dark:border-gray-700 bg-gray-50 dark:bg-gray-800 text-gray-900 dark:text-white mb-4"
+            placeholder="Enter blog description"
+            rows={4}
+            required
+          />
+          {formErrors.description && <p className="field-error">{formErrors.description}</p>}
+        </div>
 
-      <label className="block text-gray-800 dark:text-gray-200 mb-1">Choose an Image</label>
-      <div className="grid grid-cols-2 md:grid-cols-4 gap-4 mb-6">
-        {imageOptions.map((src) => (
-          <div
-            key={src}
-            onClick={() => setSelectedImage(src)}
-            className={`cursor-pointer border-4 rounded-xl overflow-hidden ${image === src
-              ? "border-blue-300 shadow-md"
-              : "border-transparent"
-              }`}
-          >
-            <Image
-              src={src}
-              alt="blog image option"
-              width={300}
-              height={200}
-              className="object-cover w-full h-28"
-            />
-          </div>
-        ))}
-      </div>
-      {formErrors.image && <p className="field-error mb-1">{formErrors.image}</p>}
+        <label className="block text-gray-800 dark:text-gray-200 mb-1">Choose an Image</label>
+        <div className="grid grid-cols-2 md:grid-cols-4 gap-4 mb-6">
+          {imageOptions.map((src) => (
+            <div
+              key={src}
+              onClick={() => setSelectedImage(src)}
+              className={`cursor-pointer border-4 rounded-xl overflow-hidden ${image === src
+                ? "border-blue-300 shadow-md"
+                : "border-transparent"
+                }`}
+            >
+              <Image
+                src={src}
+                alt="blog image option"
+                width={300}
+                height={200}
+                className="object-cover w-full h-28"
+              />
+            </div>
+          ))}
+        </div>
+        {formErrors.image && <p className="field-error mb-1">{formErrors.image}</p>}
 
-      <LoginBtn text="Submit Blog" />
-    </form>
+        <LoginBtn text="Submit Blog" />
+      </form>
+    </AuthGuard>
   );
 }
--- app/blogs/page.tsx
+++ app/blogs/page.tsx
@@ -1,26 +1,24 @@
 "use client";
 
 import BlogCard from "@/components/BlogCard";
-import { getItem } from '../utils/storage';
-import { Blog } from "@/types";
-import { sortBlogsByDate } from "../utils/date";
+import AuthGuard from "@/hooks/AuthGuard";
+import { useUserAuth } from "@/hooks/userAuth";
 
 const Blogs = () => {
 
-  const blogs = getItem<Blog[]>("blogs");
-
-  const sortedBlogs = blogs ? sortBlogsByDate(blogs) : [];
-  // console.log(sortedBlogs);
+  const { blogs } = useUserAuth(true);
 
   return (
-    <main className="p-4 max-w-4xl mx-auto">
-      <h1 className="text-3xl font-bold mb-6 text-gray-900 dark:text-white">Latest Blogs</h1>
-      <div className="grid md:grid-cols-2 lg:grid-cols-3 gap-6">
-        {sortedBlogs && sortedBlogs.map(blog => (
-          <BlogCard key={blog.id} blog={blog} />
-        ))}
-      </div>
-    </main>
+    <AuthGuard>
+      <main className="p-4 max-w-4xl mx-auto">
+        <h1 className="text-3xl font-bold mb-6 text-gray-900 dark:text-white">Latest Blogs</h1>
+        <div className="grid md:grid-cols-2 lg:grid-cols-3 gap-6">
+          {blogs && blogs.map(blog => (
+            <BlogCard key={blog.id} blog={blog} />
+          ))}
+        </div>
+      </main>
+    </AuthGuard>
   );
 }
 
